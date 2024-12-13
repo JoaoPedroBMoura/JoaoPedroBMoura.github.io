ac76fbc06d6e6297e757eac27cf8c9025279f79b
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-12-13 14:07:43 -0300

    feat: updating project page

--- pages/pages.js
+++ pages/pages.js
@@ -1,19 +1,12 @@
 import { Projeto } from "./projects.js";
 const body = document.querySelector('body')
 
-function pegarOsItensDeOutroHTML() {
-    
+function pegarOsItensDeOutroHTML() {   
     body.innerHTML = ``
-
     body.append(Projeto)
-    
-
-    
 }
 
 const linkProjeto = document.getElementById('projeto-programacao')
-console.log(linkProjeto)
-console.log(Projeto)
 
     linkProjeto.addEventListener('click', (env) => {
         
--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,55 @@
-export const Projeto = document.createElement('div')
+export const Projeto = document.createElement('main')
+Projeto.className = 'letra-branca'
 
-const h1 = document.createElement('h1')
-h1.innerText = 'Funcionou!!'
+const titulo = document.createElement('h1')
+titulo.innerText = 'Projetos'
 
-Projeto.append(h1)
+function criarLi(nomeLink,link) {
+    const liProjeto = document.createElement('li')
+    const linkSiteNatureza = document.createElement("a")
+    linkSiteNatureza.className = 'letra-branca font-media'
+    linkSiteNatureza.href = link
+    linkSiteNatureza.innerText = nomeLink
+    liProjeto.appendChild(linkSiteNatureza)
+    
+    return liProjeto;
+}
+
+function criarUl(arrayObj) {
+    const ulProjetos = document.createElement('ul')
+    
+    arrayObj.forEach(obj => {
+        
+        const linkPronto = criarLi(obj.nome, obj.link)
+         ulProjetos.append(linkPronto)
+        
+    });
+    
+    return ulProjetos
+
+}
+
+const arrayObj = []
+
+const obj = {
+    nome: "Natureza",
+    link: "https://joaopedrobmoura.github.io/Reportagens/"
+}
+
+const obj2 = {
+    nome: "Reportagem",
+    link: "https://joaopedrobmoura.github.io/Web-Fundamentals-Project/"
+}
+
+const obj3 = {
+    nome: "Site de Vendas",
+    link: "https://joaopedrobmoura.github.io/Momentos-Perfeitos/"
+}
+
+arrayObj.push(obj, obj2, obj3)
+
+const ul = criarUl(arrayObj)
+ul.className = 'no-link-style'
+
+Projeto.append(titulo, ul)
+
