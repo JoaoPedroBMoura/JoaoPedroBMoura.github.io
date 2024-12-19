fc7347207b095196c0e79247eb52dc4b85479747
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-12-19 15:04:48 -0300

    fix: Improving the list of courses. Creating Toggle List

--- pages/skills-and-courses.js
+++ pages/skills-and-courses.js
@@ -1,18 +1,15 @@
 export const skillAndCourses = document.createElement('div')
-            //  skillAndCourses.className = 'letra-branca'
+skillAndCourses.className = 'letra-branca'
 
-const titulo = document.createElement('h1')
-titulo.innerText = 'Skill e Cursos complementares'
-titulo.className = 'centro letra-branca'
-
-const tituloCarreiraProgramacao = document.createElement('h2')
-tituloCarreiraProgramacao.innerText = 'Programação'
-tituloCarreiraProgramacao.className = 'container letra-branca'
+const DB = "https://joaopedrobmoura.github.io/data"
+const estrutura = "db.json"
 
 function criarLiCursos(nomeCurso,link) {
+    console.log(`Criando LI para: ${nomeCurso}`);
     const liCurso = document.createElement('li')
-        liCurso.className ='margin-list-10'
+        liCurso.className ='margin-list-10 '
     const linkCurso = document.createElement("a")
+    linkCurso.className = 'dropdown-item'
 
     linkCurso.className = 'letra-branca font-media'
 
@@ -24,9 +21,13 @@ function criarLiCursos(nomeCurso,link) {
 }
 
 function criarUl(arrayObj) {
+    console.log(`Criando UL para cursos`)
     const ulCursos = document.createElement('ul')
-    
+    ulCursos.className = 'dropdown-menu navbar-nav'
+
     arrayObj.forEach(obj => {
+
+        console.log(obj)
         
         const linkPronto = criarLiCursos(obj.nome, obj.link)
          ulCursos.append(linkPronto)
@@ -37,20 +38,68 @@ function criarUl(arrayObj) {
 
 }
 
-async function pegarDados() {
-    const response = await fetch("https://joaopedrobmoura.github.io/data/db.json", { 
-        method: "GET"
-    }); 
+async function pegarDados(materia) {
+    try {
+        const response = await fetch(`${DB}/${estrutura}`, { 
+            method: "GET"
+        }); 
         if (response.ok) {
             const json = await response.json();
-            return json.cursos; // Extrai e retorna o array específico 
-    } else {
-        console.error("Erro ao carregar os dados JSON");
-    }
+            console.log(json[materia])
+            return json[materia]
+        } else {
+            console.error("Erro ao carregar os dados JSON");
+        }
+    } catch (error) {
+        console.error("Erro na função pegarDados", error);
+   }
+   
 }
 
+async function criaToggle(nomeToggle,curso) {
+    try {
+        const divBotao = document.createElement("div");
+        divBotao.className = 'navbar-toggler dropdown no-break letra-branca';
 
-const ulCursosProgramacao = criarUl(await pegarDados())
-ulCursosProgramacao.className = 'no-link-style container'
+        const toggleButton = document.createElement('a'); 
+        toggleButton.href = '#cursos';
+        toggleButton.className = 'dropdown-toggle letra-branca font-media'; 
+        toggleButton.setAttribute('data-toggle', 'collapse');
+        toggleButton.innerText = nomeToggle;
+        divBotao.append(toggleButton);
+        
+        const array = await pegarDados(curso)
 
-skillAndCourses.append(titulo, tituloCarreiraProgramacao, ulCursosProgramacao)
+        const ulDeProjetos = criarUl(array);
+        ulDeProjetos.id = 'cursos';
+        ulDeProjetos.className = 'collapse list-unstyled'
+        divBotao.appendChild(ulDeProjetos);
+       
+
+        return divBotao;
+    } catch(error){
+        console.error("Problema com o servidor", error);
+    }
+    
+}
+
+async function criaPagina() { 
+    const titulo = document.createElement('h1')
+    titulo.innerText = 'Skill e Cursos complementares'
+    titulo.className = 'centro letra-branca'
+
+    const tituloCarreiraProgramacao = document.createElement('h2')
+    tituloCarreiraProgramacao.innerText = 'Programação'
+    tituloCarreiraProgramacao.className = 'container letra-branca'
+    
+    const divProgramacao = document.createElement('div');
+    divProgramacao.className = 'container col-md-6 col-xs-6 col-lg-6';
+
+    const cursoDotNet = await criaToggle(".Net", "cursos");
+    cursoDotNet.className = 'padding-esquerda-20'
+
+    divProgramacao.append(tituloCarreiraProgramacao, cursoDotNet)
+    skillAndCourses.append(titulo, divProgramacao);
+}
+
+criaPagina();
