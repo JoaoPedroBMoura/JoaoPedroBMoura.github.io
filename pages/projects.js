7390caf20d690f9f9928f127251a02b9c0e6beae
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-12-21 16:43:07 -0300

    feat: adding new projects and updating page

--- pages/projects.js
+++ pages/projects.js
@@ -1,30 +1,38 @@
 import { Util } from "./utils.js";
 
 export const Projeto = document.createElement('div')
-Projeto.className = 'letra-branca'
+Projeto.className = 'container letra-branca lado-a-lado margem-cima-30'
 
 const projetos = new Util("projetosProgramacao.json");
 
 async function criaPagina() { 
+    const divSites = document.createElement('div');
+    divSites.className = 'container col-md-6 col-xs-6 col-lg-6';
+
+    const divConsoles = document.createElement('div');
+    divConsoles.className = 'container col-md-6 col-xs-6 col-lg-6';
+    
     const titulo = document.createElement('h1')
     titulo.innerText = 'Projetos'
     titulo.className = 'centro letra-branca'
 
-    const tituloProjetosProgramacao = document.createElement('h2')
-    tituloProjetosProgramacao.innerText = 'Sites'
-    tituloProjetosProgramacao.className = 'container letra-branca'
-    
-    const divProjetosProgramcao = document.createElement('div');
-    divProjetosProgramcao.className = 'container col-md-6 col-xs-6 col-lg-6';
+    const tituloProjetosSites = document.createElement('h2')
+    tituloProjetosSites.innerText = 'Sites'
+    tituloProjetosSites.className = 'container letra-branca'
 
-    const projetosProgramcao = await projetos.criaToggle("Fundamentos da Web", "sites");
-    projetosProgramcao.className = 'padding-esquerda-20'
+    const tituloProjetosConsole = document.createElement('h2')
+    tituloProjetosConsole.innerText = 'Projetos de Console'
+    tituloProjetosConsole.className = 'container letra-branca'
 
-    // const projetosDeConsole = await projetos.criaToggle("Projetos de Console", "console");
-    // projetosDeConsole.className = 'padding-esquerda-20'
+    const proejtosDeSites = await projetos.criaToggle("Fundamentos da Web", "sites");
+    proejtosDeSites.className = 'padding-esquerda-20'
 
-    divProjetosProgramcao.append(tituloProjetosProgramacao, projetosProgramcao);
-    Projeto.append(titulo, divProjetosProgramcao);
+    const projetosDeConsole = await projetos.criaToggle("Projetos de Console", "deConsole");
+    projetosDeConsole.className = 'padding-esquerda-20'
+
+    divSites.append(tituloProjetosSites, proejtosDeSites)
+    divConsoles.append(tituloProjetosConsole,projetosDeConsole)
+    Projeto.append(divSites,divConsoles);
 }
 
 criaPagina();
