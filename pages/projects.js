4c626c16e319493c83696402367417d2445cf269
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2024-12-21 16:23:32 -0300

    refactor: updating names db

--- pages/projects.js
+++ pages/projects.js
@@ -1,12 +1,9 @@
 import { Util } from "./utils.js";
 
 export const Projeto = document.createElement('div')
-Projeto.className = 'letra-branca   '
+Projeto.className = 'letra-branca'
 
-
-
-
-const projetos = new Util("projetos.json");
+const projetos = new Util("projetosProgramacao.json");
 
 async function criaPagina() { 
     const titulo = document.createElement('h1')
@@ -14,17 +11,17 @@ async function criaPagina() {
     titulo.className = 'centro letra-branca'
 
     const tituloProjetosProgramacao = document.createElement('h2')
-    tituloProjetosProgramacao.innerText = ''
+    tituloProjetosProgramacao.innerText = 'Sites'
     tituloProjetosProgramacao.className = 'container letra-branca'
     
     const divProjetosProgramcao = document.createElement('div');
     divProjetosProgramcao.className = 'container col-md-6 col-xs-6 col-lg-6';
 
-    const projetosProgramcao = await projetos.criaToggle("Fundamentos da Web", "programacao");
+    const projetosProgramcao = await projetos.criaToggle("Fundamentos da Web", "sites");
     projetosProgramcao.className = 'padding-esquerda-20'
 
-    // const gitGitHub = await projetos.criaToggle("Git e Git Hub", "gitGitHub");
-    // gitGitHub.className = 'padding-esquerda-20'
+    // const projetosDeConsole = await projetos.criaToggle("Projetos de Console", "console");
+    // projetosDeConsole.className = 'padding-esquerda-20'
 
     divProjetosProgramcao.append(tituloProjetosProgramacao, projetosProgramcao);
     Projeto.append(titulo, divProjetosProgramcao);
