21415c0a3f59a6a979bd8e1dca7b988591f5b6f4
joaoPedroBM <joaopedrobarcelllosmoura@gmail.com>
2025-01-14 11:11:31 -0300

    feat: adding new projects

--- pages/projects.js
+++ pages/projects.js
@@ -6,9 +6,11 @@ export async function criaPaginaProjeto() {
     const sectionSite = document.createElement('section');
     sectionSite.className = 'container row text-center col-md-12 col-lg-12';
 
-    const projetoNatureza = projetos.criaCard("Reportagem","projetosProgramacao.json","sites")
+    const projetoEmpresa = await projetos.criaCard("Site de Empresa","sites")
+    const projetoNatureza = await projetos.criaCard("Reportagem", "sites")
+    const projetoVendas = await projetos.criaCard("Site de Vendas","sites")
 
-    sectionSite.append(projetoNatureza)
+    sectionSite.append(projetoEmpresa,projetoNatureza,projetoVendas)
     return sectionSite;
 }
 
